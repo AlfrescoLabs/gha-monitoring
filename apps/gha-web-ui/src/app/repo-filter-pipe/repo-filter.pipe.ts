f4bb9f4bec139d6b418249af7a0dbe7812262dde
Domenico Sibilio <domenicosibilio@gmail.com>
2022-09-08 12:41:36 +0200

    Generic filter for repos

---
+++ apps/gha-web-ui/src/app/repo-filter-pipe/repo-filter.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Repo } from '../model/repo.model';
+
+@Pipe({
+  name: 'repoFilter'
+})
+export class RepoFilterPipe implements PipeTransform {
+
+  transform(repos: Array<Repo> | null, filter: string): Array<Repo> {
+    if (!repos)
+      return [];
+    if (!filter)
+      return repos;
+
+    return repos.filter(repo => {
+      return this.matchesFilter(repo, filter.toLowerCase());
+    });
+  }
+
+  private matchesFilter(repo: Repo, filter: string): boolean {
+    return repo.repo.name.toLowerCase().includes(filter) ||
+      repo.headBranch.name.toLowerCase().includes(filter) ||
+      repo.status.toLowerCase().includes(filter) ||
+      repo.commit.id.toLowerCase().includes(filter);
+  }
+
+}
