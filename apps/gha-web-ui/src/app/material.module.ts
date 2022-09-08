fc7f48e0458ba80ad042540728f89b18a55db0df
Mykyta Maliarchuk <maliarchuk99@gmail.com>
2022-09-08 17:15:11 +0200

    pagination

--- apps/gha-web-ui/src/app/material.module.ts
+++ apps/gha-web-ui/src/app/material.module.ts
@@ -5,9 +5,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
-  imports: [MatIconModule, MatInputModule, MatButtonModule, FormsModule, MatToolbarModule, MatProgressBarModule],
-  exports: [MatIconModule, MatInputModule, MatButtonModule, FormsModule, MatToolbarModule, MatProgressBarModule]
+  imports: [MatIconModule, MatInputModule, MatButtonModule, FormsModule, MatToolbarModule, MatProgressBarModule, MatPaginatorModule],
+  exports: [MatIconModule, MatInputModule, MatButtonModule, FormsModule, MatToolbarModule, MatProgressBarModule, MatPaginatorModule]
 })
 export class MaterialModule {}
