96ad2ccc73beffcf4cd601768ff5d4bc4f6de10c
Nikita Maliarchuk <84377976+nikita-web-ua@users.noreply.github.com>
2022-09-08 17:57:39 +0200

    Loading bar & Pagination  (#32)

--- apps/gha-web-ui/src/app/active-repositories/active-repositories.smart-component.ts
+++ apps/gha-web-ui/src/app/active-repositories/active-repositories.smart-component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import { ActiveRepositoriesService } from '../services/active-repositories.service';
 import { Observable } from 'rxjs';
 import { Repo } from '../model/repo.model';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'active-repositories',
@@ -14,11 +15,18 @@ export class ActiveRepositoriesSmartComponent implements OnInit {
   @Input() owner: string
   repos$: Observable<Repo[]>;
   filter: '';
+  startIndex = 0;
+  endIndex = 10;
 
   constructor(private activeRepositoriesService: ActiveRepositoriesService) {}
 
   ngOnInit(): void {
     this.repos$ = this.activeRepositoriesService.repos$
     this.activeRepositoriesService.loadRepos(this.repoList, this.owner)
+  }
+
+  onPageChange(event: PageEvent) {
+    this.startIndex = event.pageIndex * event.pageSize;
+    this.endIndex = this.startIndex + event.pageSize;
   }
 }
--- apps/gha-web-ui/src/app/material.module.ts
+++ apps/gha-web-ui/src/app/material.module.ts
@@ -3,9 +3,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
-  imports: [MatIconModule, MatInputModule, MatButtonModule, FormsModule],
-  exports: [MatIconModule, MatInputModule, MatButtonModule, FormsModule]
+  imports: [MatIconModule, MatInputModule, MatButtonModule, FormsModule, MatToolbarModule, MatProgressBarModule, MatPaginatorModule],
+  exports: [MatIconModule, MatInputModule, MatButtonModule, FormsModule, MatToolbarModule, MatProgressBarModule, MatPaginatorModule]
 })
 export class MaterialModule {}
